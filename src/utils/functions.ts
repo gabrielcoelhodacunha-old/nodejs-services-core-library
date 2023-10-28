fe237923f79c65707b25a4936dbf1f1f1e60e8cb
Gabriel Coelho da Cunha <43345507+gabrielcoelhodacunha@users.noreply.github.com>
2023-10-28 06:49:22 -0300

    [test] add tests for utils functions

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,20 @@
 import { genSalt, hash } from "bcrypt";
+import { UUID } from "mongodb";
+import { Env } from "./types";
 
 export async function hashString(str: string): Promise<string> {
   const salt = await genSalt();
   return await hash(str, salt);
+}
+
+export function isUUID(data: unknown): data is UUID {
+  return (data as UUID).toHexString !== undefined;
+}
+
+export function transformEnv(env: Env): Env {
+  return {
+    ...env,
+    BASE_URL: `${env.PROTOCOL}://${env.HOST}:${env.PORT}`,
+    MONGO_URI: `mongodb://${env.MONGO_USER}:${env.MONGO_PASSWORD}@${env.MONGO_HOST}:${env.MONGO_PORT}/${env.MONGO_DATABASE}`,
+  };
 }
--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -1,34 +1,28 @@
 import { randomUUID } from "node:crypto";
 import { z } from "zod";
 import { UUID } from "mongodb";
+import { isUUID } from "./functions";
 
 export const numberParser = z.number({ coerce: true });
 export const uuidParser = z.string().uuid().default(randomUUID);
 export const emailParser = z.string().email();
 export const dateParser = z.date().default(new Date());
 
-export const mongoUuidParser = z
-  .custom<UUID>(
-    (data): data is UUID => (data as UUID).toHexString !== undefined
-  )
-  .default(() => new UUID());
+export const mongoUuidParser = z.custom<UUID>(isUUID).default(
+  /* istanbul ignore next */
+  () => new UUID()
+);
 
-export const envParser = z
-  .object({
-    APP_NAME: z.string().default(""),
-    PROTOCOL: z.string().default("http"),
-    HOST: z.string().default("localhost"),
-    PORT: numberParser.default(3000),
-    BASE_URL: z.string().default(""),
-    MONGO_USER: z.string().default("username"),
-    MONGO_PASSWORD: z.string().default("password"),
-    MONGO_HOST: z.string().default("localhost"),
-    MONGO_PORT: numberParser.default(27017),
-    MONGO_DATABASE: z.string().default("nodejs_services"),
-    MONGO_URI: z.string().default(""),
-  })
-  .transform((env) => ({
-    ...env,
-    BASE_URL: `${env.PROTOCOL}://${env.HOST}:${env.PORT}`,
-    MONGO_URI: `mongodb://${env.MONGO_USER}:${env.MONGO_PASSWORD}@${env.MONGO_HOST}:${env.MONGO_PORT}/${env.MONGO_DATABASE}`,
-  }));
+export const envParser = z.object({
+  APP_NAME: z.string().default(""),
+  PROTOCOL: z.string().default("http"),
+  HOST: z.string().default("localhost"),
+  PORT: numberParser.default(3000),
+  BASE_URL: z.string().default(""),
+  MONGO_USER: z.string().default("username"),
+  MONGO_PASSWORD: z.string().default("password"),
+  MONGO_HOST: z.string().default("localhost"),
+  MONGO_PORT: numberParser.default(27017),
+  MONGO_DATABASE: z.string().default("nodejs_services"),
+  MONGO_URI: z.string().default(""),
+});
